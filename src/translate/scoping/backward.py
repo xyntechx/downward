f842730e0f6044fd82718328860e15d7e2c3d564
Cam Allen <camallen@berkeley.edu>
2024-09-20 01:50:31 -0700

    [WIP] towards causal links

--- src/translate/scoping/backward.py
+++ src/translate/scoping/backward.py
@@ -2,7 +2,7 @@
 #!%cd ~/dev/downward/src/translate
 
 # %%
-from typing import List, Set, Tuple
+from typing import Any, List, Set, Tuple
 
 import pddl_parser
 
@@ -15,19 +15,19 @@ from scoping.sas_parser import SasParser
 from translate import pddl_to_sas
 
 # %%
-domain_filename = "../../../scoping/domains/propositional/ipc/gripper/domain.pddl"
-task_filename = "../../../scoping/domains/propositional/ipc/gripper/prob04.pddl"
+# domain_filename = "../../../scoping/domains/propositional/ipc/gripper/domain.pddl"
+# task_filename = "../../../scoping/domains/propositional/ipc/gripper/prob04.pddl"
 
 # domain_filename = (
 #     "../../../scoping/domains/propositional/toy-minecraft/toy-example.pddl"
 # )
 # task_filename = "../../../scoping/domains/propositional/toy-minecraft/example-1.pddl"
-options.keep_unimportant_variables = True
-options.sas_file = True
-task = pddl_parser.open(domain_filename, task_filename)
-sas_task: SASTask = pddl_to_sas(task)
+# options.keep_unimportant_variables = True
+# options.sas_file = True
+# task = pddl_parser.open(domain_filename, task_filename)
+# sas_task: SASTask = pddl_to_sas(task)
 
-sas_task.dump()
+# sas_task.dump()
 
 # %%
 sas_path = "../../toy-minecraft.sas"
@@ -36,16 +36,35 @@ parser.parse()
 sas_task: SASTask = parser.to_fd()
 
 
+def filter_unthreatened_causal_links(
+    facts: FactSet, init: List[Tuple[Any, Any]], actions: Set[VarValAction]
+) -> FactSet:
+    affected_facts = FactSet()
+    for a in actions:
+        affected_facts.add(a.effects)
+    unthreatened_init_facts = FactSet(
+        [(var, val) for (var, val) in init if affected_facts[var] in [set(), set(val)]]
+    )
+    relevant_facts = FactSet()
+    for var, values in facts:
+        for val in values:
+            if (var, val) not in unthreatened_init_facts:
+                relevant_facts.add((var, val))
+    return relevant_facts
+
+
 def get_backward_reachable_actions(
     facts: FactSet, actions: Set[VarValAction]
 ) -> Set[VarValAction]:
-    return set([a for a in actions for fact in a.precondition if fact in facts])
+    return set([a for a in actions for fact in a.effects if fact in facts])
 
 
 def get_backward_reachable_facts(
     variable_domains: FactSet,
     reachable_actions: List[VarValAction],
+    init: List[Tuple[Any, Any]],
     enable_merging: bool = False,
+    enable_causal_links: bool = False,
 ) -> FactSet:
     if enable_merging:
         # partition actions by (effect, cost)
@@ -64,7 +83,6 @@ def get_backward_reachable_facts(
         reachable_facts = FactSet()
         for partition_factset in reachable_factsets:
             reachable_facts.union(partition_factset)
-        return reachable_facts
     else:
         reachable_facts = FactSet()
         for action in reachable_actions:
@@ -73,18 +91,45 @@ def get_backward_reachable_facts(
                     reachable_facts.union(var, variable_domains[var])
                 else:
                     reachable_facts.add(var, val)
-        return reachable_facts
+
+    # if not enable_causal_links:
+    #     relevant_facts = reachable_facts
+    # else:
+    #     affected_facts = FactSet()
+    #     for a in reachable_actions:
+    #         affected_facts.add(a.effects)
+    #     unthreatened_init_facts = FactSet(
+    #         [
+    #             (var, val)
+    #             for (var, val) in init
+    #             if affected_facts[var] in [set(), set([val])]
+    #         ]
+    #     )
+    #     relevant_facts = FactSet()
+    #     for var, values in reachable_facts:
+    #         for val in values:
+    #             if (var, val) not in unthreatened_init_facts:
+    #                 relevant_facts.add(var, val)
+
+    return reachable_facts
 
 
 def backward_reachability_step(
     variable_domains: FactSet,
     facts: FactSet,
+    init: List[Tuple[Any, Any]],
     actions: Set[VarValAction],
     enable_merging: bool = False,
+    enable_causal_links: bool = False,
 ) -> Tuple[FactSet, Set[VarValAction]]:
+    # filtered_facts = filter_unthreatened_causal_links(facts, init, actions)
     reachable_actions = get_backward_reachable_actions(facts, actions)
     reachable_facts = get_backward_reachable_facts(
-        variable_domains, reachable_actions, enable_merging=enable_merging
+        variable_domains,
+        reachable_actions,
+        init,
+        enable_merging=enable_merging,
+        enable_causal_links=enable_causal_links,
     )
     reachable_facts.union(facts)
 
@@ -92,18 +137,21 @@ def backward_reachability_step(
 
 
 def backward_reachability(
-    sas_task: SASTask, enable_merging: bool = False
+    sas_task: SASTask,
+    enable_merging: bool = False,
+    enable_causal_links: bool = False,
 ) -> Tuple[FactSet, List[VarValAction]]:
     actions = set([VarValAction.from_sas(op) for op in sas_task.operators])
     variable_domains = FactSet(
         {i: set(range(r)) for i, r in enumerate(sas_task.variables.ranges)}
     )
+    init = list(enumerate(sas_task.init.values))
     facts = FactSet(sas_task.goal.pairs)
     prev_facts = None
     while facts != prev_facts:
         prev_facts = facts
         facts, reachable_actions = backward_reachability_step(
-            variable_domains, facts, actions, enable_merging
+            variable_domains, facts, init, actions, enable_merging, enable_causal_links
         )
         facts
 
@@ -111,8 +159,14 @@ def backward_reachability(
 
 
 # %%
-
-facts, actions = backward_reachability(sas_task, enable_merging=False)
-print("actions:", len(actions))
-print("facts:")
-sorted(facts)
+for merging in [False, True]:
+    for causal_links in [False, True]:
+        facts, actions = backward_reachability(
+            sas_task,
+            enable_merging=merging,
+            enable_causal_links=causal_links,
+        )
+        print(f"{merging=}, {causal_links=}")
+        print("actions:", len(actions), sorted(a.name for a in actions))
+        print("facts:", sorted(facts))
+        print()
